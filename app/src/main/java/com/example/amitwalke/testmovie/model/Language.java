488a35052c485546ae8edcc4c6aeae9143ecc935
amitwalke <walkeamit252@gmail.com>
2018-12-03 16:59:14 +0530

    Changes done

--- app/src/main/java/com/example/amitwalke/testmovie/model/Language.java
+++ app/src/main/java/com/example/amitwalke/testmovie/model/Language.java
@@ -2,10 +2,6 @@ package com.example.amitwalke.testmovie.model;
 
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by bharatsupekar on 10/24/18.
- */
-
 public class Language {
     @SerializedName("name")
     private String name;
